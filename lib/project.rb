98f8ff4be7c852fdb1e9369b2fe45906b228ed67
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-02 02:22:55 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,17 @@
+class Project
+
+  attr_accessor :title
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
+
+  def backers
+    @backers
+  end
+end
